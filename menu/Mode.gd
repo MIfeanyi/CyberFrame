8b40d3d6df424ce944995552278eee6c1f20a1c0
Michael Redford <michaeliredford@gmail.com>
2020-02-19 15:10:54 +0900

    new menu
    Signed-off-by: Michael Redford <michaeliredford@gmail.com>

---
+++ menu/Mode.gd
@@ -0,0 +1,13 @@
+extends OptionButton
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	add_item("Select Mode")
+	add_item("Versus")
+	add_item("Time Limit")
+	add_item("Survival")
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
